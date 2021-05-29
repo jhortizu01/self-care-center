e73549b23f0297f2522b7394c766100682346e38
Janika Hortizuela <jhortizuela21@gmail.com>
2021-05-29 14:28:04 -0600

    Add login page

--- main.js
+++ main.js
@@ -4,16 +4,32 @@ var newMessageButton = document.querySelector(".new-message");
 var randomMessages = document.querySelector(".random-messages")
 var chooseAffirmations = document.getElementById("affirmations")
 var chooseMantras = document.getElementById("mantras")
-var mainPage = document.querySelector('.the-whole-page')
+var mainPage = document.querySelector(".the-whole-page")
+var loginPage = document.querySelector(".login-page")
+var submit = document.getElementById("login-button")
+var loginName = document.getElementById("login-name")
+var welcome = document.querySelector(".welcome")
+
 
 // 👂🏽Event Listeners 👂
-window.addEventListener("load", hideMainPage);
 window.addEventListener("load", hideSaying);
 newMessageButton.addEventListener("click", hideImage)
 newMessageButton.addEventListener("click", showSaying)
-newMessageButton.addEventListener("click", showSayings)
+newMessageButton.addEventListener("click", showRandomSayings)
+submit.addEventListener("click", letsLogIn)
 
 // 🤷🏽‍♀️ Functions 🤷🏽‍♀️
+
+
+function letsLogIn(){
+  event.preventDefault();
+  mainPage.classList.remove("hidden")
+  loginPage.classList.add("hidden")
+  var theirName = loginName.value
+  welcome.innerText = `Welcome to your new inner-calm, ${theirName} !`;
+}
+
+
 function hideMainPage() {
   mainPage.classList.add("hidden")
 }
@@ -34,15 +50,13 @@ function showSaying(){
   randomMessages.classList.remove("hidden")
 }
 
-function showSayings() {
+function showRandomSayings() {
   if (chooseAffirmations.checked === true){
   randomMessages.textContent = affirmations[getRandomIndex(affirmations)]
   } else if (chooseMantras.checked === true) {
   randomMessages.textContent = mantras[getRandomIndex(mantras)]
   }
 }
-
-
 
 
 
